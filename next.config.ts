ba352ade97f3293158de19db8b0ce8ceaccbd877
Asif Anjum <asif.anjum.rabi@gmail.com>
2025-04-14 11:33:49 +0600

    webhook synced

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "https://vocal-easy-foal.ngrok-free.app",
+    "*https://vocal-easy-foal.ngrok-free.app",
+  ],
 };
 
 export default nextConfig;
